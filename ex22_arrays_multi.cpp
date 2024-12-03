0e3f95bf832537af55d5d1489e11c45ab6dab628
Straquicini <a14642@oficina.pt>
2024-12-03 11:44:23 +0000

    Add files via upload

--- ex22_arrays_multi.cpp
+++ ex22_arrays_multi.cpp
@@ -8,40 +8,50 @@ string tabuleiro[numColunas] [numLinhas];
 string jogador1 = "X", jogador2 = "O";
 int quemJoga = 0;
 
+
 /**MOSTRA TABULEIRO**/
 void mostraTabuleiro(){
-    cout << "\n-------------------------------------";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n| x | x | x | x | x | x | x | x | x |";
-    cout << "\n-------------------------------------";
-    cout << "\n| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |";
-
-
-    /*for(int l = 0; l < numLinhas; l++){
-        for(int c = 0; c < numColunas; c++){
-            cout << tabuleiro[l][c];
-            if(c < 9)
-                cout << "\n| x | x | x | x | x | x | x | x | x |";
-        }
-        if(l < 6)
-            cout << "\n-------------------------------------";
-            cout << "\n| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |";
-            //cout << "\n";
-    }*/
-
-
+    cout << "-------------------\n";
+    cout << "|" << tabuleiro [0][5] << "|" << tabuleiro [1][5] << "|" << tabuleiro [2][5] << "|" << tabuleiro [3][5] << "|" << tabuleiro [4][5] << "|" << tabuleiro [5][5] << "|" << tabuleiro [6][5] << "|" << tabuleiro [7][5] << "|" << tabuleiro [8][5] << "|\n";
+    cout << "|" << tabuleiro [0][4] << "|" << tabuleiro [1][4] << "|" << tabuleiro [2][4] << "|" << tabuleiro [3][4] << "|" << tabuleiro [4][4] << "|" << tabuleiro [5][4] << "|" << tabuleiro [6][4] << "|" << tabuleiro [7][4] << "|" << tabuleiro [8][4] << "|\n";
+    cout << "|" << tabuleiro [0][3] << "|" << tabuleiro [1][3] << "|" << tabuleiro [2][3] << "|" << tabuleiro [3][3] << "|" << tabuleiro [4][3] << "|" << tabuleiro [5][3] << "|" << tabuleiro [6][3] << "|" << tabuleiro [7][3] << "|" << tabuleiro [8][3] << "|\n";
+    cout << "|" << tabuleiro [0][2] << "|" << tabuleiro [1][2] << "|" << tabuleiro [2][2] << "|" << tabuleiro [3][2] << "|" << tabuleiro [4][2] << "|" << tabuleiro [5][2] << "|" << tabuleiro [6][2] << "|" << tabuleiro [7][2] << "|" << tabuleiro [8][2] << "|\n";
+    cout << "|" << tabuleiro [0][1] << "|" << tabuleiro [1][1] << "|" << tabuleiro [2][1] << "|" << tabuleiro [3][1] << "|" << tabuleiro [4][1] << "|" << tabuleiro [5][1] << "|" << tabuleiro [6][1] << "|" << tabuleiro [7][1] << "|" << tabuleiro [8][1] << "|\n";
+    cout << "|" << tabuleiro [0][0] << "|" << tabuleiro [1][0] << "|" << tabuleiro [2][0] << "|" << tabuleiro [3][0] << "|" << tabuleiro [4][0] << "|" << tabuleiro [5][0] << "|" << tabuleiro [6][0] << "|" << tabuleiro [7][0] << "|" << tabuleiro [8][0] << "|\n";
+    cout << "-------------------";
+    cout << "\n|1|2|3|4|5|6|7|8|9|";
+    cout << "\n";
 }
 
 /** função que vai colocar a matriz/array vazia */
 void recomecarJogo(){
+    for (int j=0; j<numLinhas; j++){
+        for (int i=0; i<numColunas; i++){
+            tabuleiro[i][j]= " ";
+        }
+    }
+
+}
+
+/** controlar quem joga*/
+void jogadores(){
+    /*if (quemJoga = jogador1) {
+        cout << "Jogador X faça sua jogada: ";
+        cin >> tabuleiro[i][j];
+    }
+    if (quemJoga = jogador2){
+        cout << "Jogador O faça sua jogada: ";
+        cin >> tabuleiro[i][j];
+    }*/
+
+
+
+
 }
 
 /** função que vai verificar se o tabuleiro não tiver mais posições para jogar */
 bool tabuleiroCheio(){
+
     return false;
 }
 
@@ -61,6 +71,9 @@ string quemganhou(string fichaGanhadora){
 
 int main(){
     int op, aux;
+
+    recomecarJogo();
+
 
     mostraTabuleiro();
 
